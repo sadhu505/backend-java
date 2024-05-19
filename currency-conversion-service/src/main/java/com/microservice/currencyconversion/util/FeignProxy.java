5344487934cf123b73a1cbc71b799f41b6c77479
sadhu505 <sudipto505@gmail.com>
2024-05-19 18:42:15 +0530

    eureka naming server added

--- currency-conversion-service/src/main/java/com/microservice/currencyconversion/util/FeignProxy.java
+++ currency-conversion-service/src/main/java/com/microservice/currencyconversion/util/FeignProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservice.currencyconversion.service.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+//@RibbonClient(name = "currency-exchange-service")
 public interface FeignProxy {
 
 	@GetMapping("/exchange-service/from/{from}/to/{to}")
--- currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,11 +2,13 @@ package com.microservice.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
